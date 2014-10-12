b5aac909604aa9fbb65dd2df707d1d4180143e4c
yang-wei <yangweilim21@gmail.com>
2014-10-12 16:07:04 +0900

    add spinner

--- scripts/directives/spinner.js
+++ scripts/directives/spinner.js
@@ -1,9 +1,9 @@
 app.factory('ProgressIndicator', function() {
     var opts = {
-        lines: 11, // The number of lines to draw
+        lines: 9, // The number of lines to draw
         length: 30, // The length of each line
-        width: 15, // The line thickness
-        radius: 40, // The radius of the inner circle
+        width: 5, // The line thickness
+        radius: 10, // The radius of the inner circle
         corners: 1, // Corner roundness (0..1)
         rotate: 0, // The rotation offset
         color: '#000', // #rgb or #rrggbb
@@ -13,11 +13,11 @@ app.factory('ProgressIndicator', function() {
         hwaccel: true, // Whether to use hardware acceleration
         className: 'spinner', // The CSS class to assign to the spinner
         zIndex: 2e9, // The z-index (defaults to 2000000000)
-        top: 'auto', // Top position relative to parent in px
-        left: 'auto' // Left position relative to parent in px
+        top: '50%', // Top position relative to parent in px
+        left: '50%' // Left position relative to parent in px
     };
  
-    var target = document.getElementById('progressIndicator');
+    var target = document.querySelector('body');
     var spinner = new Spinner(opts);
  
     return {
@@ -26,9 +26,7 @@ app.factory('ProgressIndicator', function() {
             spinner.spin(target);
         },
         stopSpinner:function () {
-            $(target).fadeOut('fast');
             spinner.stop();
- 
         },
         getSpinnerTarget:function () {
             return target;
